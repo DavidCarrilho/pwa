1281a0f8b7a776efc105de3c0af5fd680d77f8de
LuanDavidCarrilho <luan.david.oliver@gmail.com>
2019-09-09 17:26:58 -0300

    first commit

---
+++ public/src/js/app.js
@@ -0,0 +1,23 @@
+
+var deferredPrompt;
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    });
+}
+
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
+
+setTimeout(function() {
+  console.log('This is executed once the timer is done!');
+}, 3000);
+
+console.log('This is executed right after setTimeout()');
---
+++ public/src/js/feed.js
@@ -0,0 +1,30 @@
+var shareImageButton = document.querySelector('#share-image-button');
+var createPostArea = document.querySelector('#create-post');
+var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+
+function openCreatePostModal() {
+  createPostArea.style.display = 'block';
+  if (deferredPrompt !== 'undefined' && deferredPrompt !== null) {
+    deferredPrompt.prompt();
+
+    deferredPrompt.userChoice.then(function(choiceResult) {
+      console.log(choiceResult.outcome);
+
+      if (choiceResult.outcome === 'dismissed') {
+        console.log('User cancelled installation');
+      } else {
+        console.log('User added to home screen');
+      }
+    });
+
+    deferredPrompt = null;
+  }
+}
+
+function closeCreatePostModal() {
+  createPostArea.style.display = 'none';
+}
+
+shareImageButton.addEventListener('click', openCreatePostModal);
+
+closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
